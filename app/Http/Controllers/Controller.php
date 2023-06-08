318b62aa35edf8407b9bea0ba3c5f6fe28c27b95
Christian <c.hoffmann@roots48.com>
2023-06-08 12:34:06 +0200

    Login breeze + node implementiert

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\DB;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+
+
+
+    function testInteractWithDB() {
+        $users = DB::select('select * from testtable');
+       
+        return view('welcome', $data = [
+            'users' => $users
+        ]);
+    }
 }
